2e4b814c4eabacdad1affd10292254143d7a84fa
“l-sri” <reach.srilakshmil@gmail.com>
2023-12-20 18:31:17 +0000

    removed some hardcoded entities and ran test from end to end

--- pdf_parser.py
+++ pdf_parser.py
@@ -8,9 +8,9 @@ from utils.docAI_extraction import process_local_document_in_chunks
 from utils.table_extraction import get_latex_tables_camelot, get_pdf_table_latex, extract_and_clean_tables
 # import argo_utils
 
-# cred_path = "llmapps-creds.json"
-cred_path = "docai-creds.json"
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = cred_path
+# # cred_path = "llmapps-creds.json"
+# cred_path = "docai-creds.json"
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = cred_path
 
 
 if __name__ == "__main__":
@@ -25,7 +25,8 @@ if __name__ == "__main__":
     #     pdf_folders = args.pdf_folders
 
     pdf_folders = [str(arg) for arg in sys.argv[1:]]
-    # print(pdf_folders)
+    pdf_folders.append("./data/input/")
+    print(pdf_folders)
     project_id = "llmapps"
     location = "us"
     processor_id = "c0ec8848a9756a33"
@@ -44,6 +45,7 @@ if __name__ == "__main__":
             pdf_sample_size = len(pdf_files)
 
             for pdf_file in pdf_files[:pdf_sample_size]:
+                print("Processing: ", pdf_file)
                 document_hash = pdf_file.split('.pdf')[0]
                 file_path = os.path.join(pdf_folder, pdf_file)
                 # latex_tables = get_latex_tables_camelot(file_path)
@@ -55,14 +57,13 @@ if __name__ == "__main__":
                 # output_path = pdf_output_folder + document_hash + '.txt'#"last_4_pages.txt"
                 # cnt = 1
 
-                output_path = pdf_output_folder + document_hash + "_last_4_pages.txt"
-                cnt = 16
-                print(len(doc_content))
+                output_path = pdf_output_folder + document_hash + ".txt"
+                print(f"Number of pages in {pdf_file}: {len(doc_content)}")
                 file = open(output_path, "w")
                 
                 # for dc in doc_content:
-                for dc in doc_content[-4:]:
-                    file.write(f"Page Number {cnt}:\n")
+                for i, dc in enumerate(doc_content):
+                    file.write(f"Page Number {i+1}:\n")
                     file.write("\n")
                     for i in range(len(dc)):#item in dc:
                         item = dc[i]
@@ -78,7 +79,6 @@ if __name__ == "__main__":
                         file.write("\n")
                         # print(item)
                     file.write("\n")
-                    cnt+=1
                 file.close()
 
 
